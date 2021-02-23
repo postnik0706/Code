28de2e469632050fe3188a218a244531bf2f1253
Alexandr Postnikov <postnik0706@hotmail.com>
2021-02-23 06:19:37 -0500

    Progress

---
+++ Chapter05/PacktLibrary9/FlightPatterns.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Packt.Shared
+{
+    public class BusinessClassPassenger
+    {
+        public override string ToString()
+        {
+            return $"Business class";
+        }
+    }
+
+    public class FirstClassPassenger
+    {
+        public int AirMiles { get; set; }
+        public override string ToString()
+        {
+            return $"First class with {AirMiles:N0} air miles";
+        }
+    }
+
+    public class CoachClassPassender
+    {
+        public double CarryOnKg { get; set; }
+        public override string ToString()
+        {
+            return $"Coach Class with {CarryOnKg:N2} KG carry on";
+        }
+    }
+}
---
+++ Chapter05/PacktLibrary9/Records.cs
@@ -0,0 +1,17 @@
+namespace Packt.Shared 
+{
+    public class ImmutablePerson
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+
+    public record ImmutableVehicle
+    {
+        public int Wheels { get; init; }
+        public string Color { get; init; }
+        public string Brand { get; init; }
+    }
+
+    public record ImmutableAnimal(string Name, string Species);
+}
